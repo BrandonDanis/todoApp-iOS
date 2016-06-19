6a996202e787f2e54b1a3580fe720bfe73896ef0
Rushil Perera <pererarushil1940@gmail.com>
2016-06-19 13:38:16 -0400

    View for todo

---
+++ TodoApp/TodoTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  TodoCellTableViewCell.swift
+//  TodoApp
+//
+//  Created by Rushil Perera on 2016-06-18.
+//  Copyright © 2016 RBSoftware. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var todo : Todo? {
+        didSet {
+            if let todoModel = todo {
+                titleLabel.text = todoModel.title
+                dateLabel.text = todoModel.date
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
