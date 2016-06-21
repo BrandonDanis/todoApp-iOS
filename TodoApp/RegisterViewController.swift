e0d1bb4010347ca7277dbcbd8dd9ccb21d7efd76
Brandon Danis <brandon@spottedu.co>
2016-06-20 23:54:46 -0400

    account creating functionality done, still needs refining

--- TodoApp/RegisterViewController.swift
+++ TodoApp/RegisterViewController.swift
@@ -8,9 +8,17 @@
 
 import Foundation
 import UIKit
+import SwiftyJSON
 
 class RegisterViewController : UIViewController
 {
+    
+    @IBOutlet weak var usernameTextbox: UITextField!
+    @IBOutlet var passwordTextbox: UITextField!
+    @IBOutlet var confirmPasswordTextbox: UITextField!
+    @IBOutlet var errorMessage: UILabel!
+    
+    let api: TodoAPI = TodoAPI(url: "https://brandon-todo.herokuapp.com")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +31,47 @@ class RegisterViewController : UIViewController
         // Dispose of any resources that can be recreated.
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    @IBAction func attemptingToRegister(sender: AnyObject) {
         
+        if(self.usernameTextbox.text == ""){
+            displayError("Must enter a username...")
+            return
+        }else if(self.passwordTextbox.text == ""){
+            displayError("Must enter a password...")
+            return
+        }else if(self.passwordTextbox.text != self.confirmPasswordTextbox.text){
+            displayError("Passwords don't match...")
+            return
+        }
+        
+        self.errorMessage.text = "Creating account..."
+        
+        let username = self.usernameTextbox.text!
+        let password = self.passwordTextbox.text!
+        
+        api.addUser(username, password: password, completion: { (response: JSON) in
+          
+            print(response)
+            
+            if(response["status"] == 200){
+                self.displayError("Account created")
+            }else{
+                self.displayError(String(response["status"]))
+            }
+            
+        })
+        
+    }
+    
+    func displayError(msg: String){
+        self.errorMessage.text = msg
+        UIView.animateWithDuration(1.0, animations: {
+            self.errorMessage.alpha = 1.0
+            
+            UIView.animateWithDuration(1.0, delay: 2.0, options: .CurveEaseIn, animations: {
+                self.errorMessage.alpha = 0.0
+                }, completion: nil)
+        })
     }
     
 }
